1bb717252ca5b8dc4d012434fcb0d62401a9c5a9
Shay Guedj <shay.devops2024@gmail.com>
2026-05-14 16:50:31 +0300

    added terraform modules for small project

---
+++ terraform/environments/small/option-a/variables.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "subnet_a_cidr" {
+  type = string
+}
+
+variable "subnet_b_cidr" {
+  type = string
+}
+
+variable "ssh_allowed_cidr" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
